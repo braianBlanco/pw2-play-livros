8e581e3d9bb4f967cf94aec70302e477fb691592
braianBlanco <braian.blanco073@gmail.com>
2026-03-16 08:19:21 -0300

    Criação da classe Livro

---
+++ src/Livro.java
@@ -0,0 +1,12 @@
+import java.time.LocalTime;
+
+public class Livro {
+    int Paginas;
+    String Autor;
+    String Narrador;
+    String versao;
+    String Editora;
+    LocalTime Duracao;
+    double Preco;
+
+}
